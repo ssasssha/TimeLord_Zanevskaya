45dcf2ddf28263c283d7cb55e12e3ebad9d6179c
sasha <yanguzovgr@gmail.com>
2023-11-18 07:39:45 +0500

    Выполненная программа на оценку 3. Заневская.

--- Pages/Stopwatch.xaml.cs
+++ Pages/Stopwatch.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimeLord_Zanevskaya.Pages
 {
@@ -20,9 +21,54 @@ namespace TimeLord_Zanevskaya.Pages
     /// </summary>
     public partial class Stopwatch : Page
     {
+        public DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public float full_second = 0;
+        public bool start_stopwatch = false;
         public Stopwatch()
         {
             InitializeComponent();
+
+            dispatcherTimer.Tick += TimerSecond;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
         }
+
+        private void TimerSecond(object sender, EventArgs e)
+        {
+            full_second++;
+
+            float hours = (int)(full_second / 60 / 60);
+            float minuts = (int)(full_second / 60) - (hours * 60);
+            float seconds = full_second - (hours * 60 * 60) - (minuts * 60);
+
+            string s_seconds = seconds.ToString();
+            if (seconds < 10) s_seconds = "0" + seconds;
+
+            string s_minuts = minuts.ToString();
+            if (minuts < 10) s_minuts = "0" + minuts;
+
+            string s_hours = hours.ToString();
+            if (hours < 10) s_hours = "0" + hours;
+
+            time.Content = s_hours + ":" + s_minuts + ":" + s_seconds;
+        }
+
+        private void StartStopwatch(object sender, RoutedEventArgs e)
+        {
+            if(start_stopwatch == false)
+            {
+                full_second = 0;
+                dispatcherTimer.Start();
+                start_stopwatch = true;
+                start.Content = "Стоп";
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                start_stopwatch = false;
+                start.Content = "Начать";
+            }
+        }
+
+        
     }
 }
